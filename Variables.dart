e0f8d97fca3847f75c1c22adea6c7967a5fb3764
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-03-04 17:12:21 +0500

    commit

---
+++ Variables.dart
@@ -0,0 +1,25 @@
+// Variables (4th April, 2022)
+
+void main() {
+  int age = 22; // int = for integers
+  double price = 86.5; // double = floating point numbers
+  String name = "Shahzain Ahmed"; // String = for strings
+  bool isloggedin = true; // for true or false
+
+  print(age + price);
+  print(name);
+  print(isloggedin);
+  
+  // Now creating variables without the data type. 
+  // Using 'var' keyword, it will automatically detect the datatype of the variable.
+  
+  var myName = "Shahzain Ahmed";
+  var myID = 86;
+  var myLoggedIn = true;
+  var myFloat = 86.4;
+  
+  print (myName);
+  print (myID);
+  print (myLoggedIn);
+  print (myFloat);
+}
