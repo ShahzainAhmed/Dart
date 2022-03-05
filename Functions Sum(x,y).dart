ca500561f55ee24d46391ef0114cb41531eb2263
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-03-05 12:00:18 +0500

    commit

---
+++ Functions Sum(x,y).dart
@@ -0,0 +1,13 @@
+// Functions - 5th March 2022
+
+void main(){
+  sum(86,10);
+}
+
+void sum(int num1, int num2){
+  int result = num1 + num2;
+  print("The sum of $num1 and $num2 is $result");
+}
+
+// Output:
+// The sum of 86 and 10 is 96
