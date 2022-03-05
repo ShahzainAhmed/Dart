952de07eb156846fa88c08726f8d75fc78162a19
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-03-05 11:59:36 +0500

    commit

---
+++ Functions Sum( ).dart
@@ -0,0 +1,17 @@
+// Functions - 5th March 2022
+
+void main() {
+  sum(); // calling the sum() function in main method
+}
+
+// Creating a function named sum()
+void sum() {
+  int num1 = 86;
+  int num2 = 10;
+  
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: ${num1+num2}");
+}
+
+// Output:
+// The sum of 86 and 10 is: 96
