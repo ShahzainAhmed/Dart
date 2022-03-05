e8351e1fcf6db1a4558a78462f8debafd3316514
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-03-05 08:11:40 +0500

    commit

---
+++ forAndforin(Loops).dart
@@ -0,0 +1,17 @@
+// Loops - 5th March 2022
+// for loop and for in loop
+
+void main() {
+  
+  // for loop
+  for (var i = 1; i <= 10; i++ ) {
+    print(i);
+  }
+  
+  // for in loop
+  List colors = ['Red', 'Green', 'Blue'];
+  for (String storeColors in colors) {
+    print(storeColors);
+  }
+ 
+}
